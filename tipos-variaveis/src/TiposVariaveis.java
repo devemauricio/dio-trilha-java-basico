751880e4953c30f42a8f16592ea1e3c3bf5a5a07
Mauricio Deve <deve.mauricio@gmail.com.br>
2024-05-25 08:58:41 -0300

    Inversão de valores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
+        numero = - numero;
+        System.out.println(numero);
     }
 }
