96e264442a3a3ef52485aa116b91c95e1409d0aa
Mauricio Deve <deve.mauricio@gmail.com.br>
2024-05-25 09:00:05 -0300

    Operador ternário

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        int numero = 5;
-        numero = - numero;
-        System.out.println(numero);
-        numero = - numero;
-        System.out.println(numero);
+        int a = 5;
+        int b = 6;
+        
+        String resultado = a == b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
     }
 }
